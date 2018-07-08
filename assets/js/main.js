7069f2e996847becee79333731b118524034b50d
Sam Gal <aamer200268@gmail.com>
2018-07-08 20:08:41 +0200

    Use Mapbox instead of Google Maps.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,15 @@
 let restaurants,
   neighborhoods,
   cuisines
-var map
+// var map  google map
+var newMap // leaflet map
 var markers = []
 
 /**
  * Fetch neighborhoods and cuisines as soon as the page is loaded.
  */
 document.addEventListener('DOMContentLoaded', (event) => {
+  initMap(); // added
   fetchNeighborhoods();
   fetchCuisines();
 });
@@ -68,9 +70,29 @@ fillCuisinesHTML = (cuisines = self.cuisines) => {
 }
 
 /**
+ * Initialize leaflet map, called from HTML.
+ */
+initMap = () => {
+    self.newMap = L.map('map', {
+          center: [40.722216, -73.987501],
+          zoom: 12,
+          scrollWheelZoom: false
+        });
+    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
+      mapboxToken: 'pk.eyJ1IjoiZWxxbm55IiwiYSI6ImNqamQxNHFwdTAyMXYzcHBhaHpwaGVwYTEifQ.FKXd-SMgNLFqFxyPmSB6yQ',
+      maxZoom: 18,
+      attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
+        '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+        'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+      id: 'mapbox.streets'
+    }).addTo(newMap);
+
+    updateRestaurants();
+  }
+  /**
  * Initialize Google map, called from HTML.
  */
-window.initMap = () => {
+  /* window.initMap = () => {
   let loc = {
     lat: 40.722216,
     lng: -73.987501
@@ -81,7 +103,7 @@ window.initMap = () => {
     scrollwheel: false
   });
   updateRestaurants();
-}
+} */
 
 /**
  * Update page and map for current restaurants.
@@ -116,7 +138,10 @@ resetRestaurants = (restaurants) => {
   ul.innerHTML = '';
 
   // Remove all map markers
-  self.markers.forEach(m => m.setMap(null));
+  // self.markers.forEach(m => m.setMap(null)); For google map
+  if (self.markers) { // For leaflet map
+    self.markers.forEach(marker => marker.remove());
+  }
   self.markers = [];
   self.restaurants = restaurants;
 }
@@ -206,7 +231,19 @@ createRestaurantHTML = (restaurant) => {
 /**
  * Add markers for current restaurants to the map.
  */
-addMarkersToMap = (restaurants = self.restaurants) => {
+addMarkersToMap = (restaurants = self.restaurants) => { // For leaflet
+  restaurants.forEach(restaurant => {
+    // Add marker to the map
+    const marker = DBHelper.mapMarkerForRestaurant(restaurant, self.newMap);
+    marker.on("click", onClick);
+    function onClick() {
+      window.location.href = marker.options.url;
+    }
+    self.markers.push(marker);
+  });
+
+}
+/* addMarkersToMap = (restaurants = self.restaurants) => {
   restaurants.forEach(restaurant => {
     // Add marker to the map
     const marker = DBHelper.mapMarkerForRestaurant(restaurant, self.map);
@@ -215,7 +252,7 @@ addMarkersToMap = (restaurants = self.restaurants) => {
     });
     self.markers.push(marker);
   });
-}
+} */
 // For Accessibility
 // remove focus from google map for keyboard users.
 document.querySelector('a').addEventListener('keydown', skipMap);
